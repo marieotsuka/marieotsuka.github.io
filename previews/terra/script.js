487c362f12d81f7376ce4264c7371819814dc649
Marie Otsuka <marieotsuka@users.noreply.github.com>
2018-12-20 18:14:23 +0000

    Add files via upload

--- previews/terra/script.js
+++ previews/terra/script.js
@@ -12,56 +12,30 @@ onYouTubeIframeAPIReady = function () {
     player = new YT.Player('player', {
         width: '600',
         height: '400',
-        videoId: 'EnyfHTJVzh8',  // youtube video id
+        videoId: 'zS3rdqU9d50',  // youtube video id
         playerVars: {
             'autoplay': 0,
             'rel': 0,
             'showinfo': 0
         },
         events: {
-            // 'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
         }
     });
 }
 
-// function onPlayerReady(event) {
-//   event.target.playVideo();
-// }
 
 var done = false;
 
 function onPlayerStateChange(event) {
     if (event.data == YT.PlayerState.ENDED) {
             $("#thumbnail_container").fadeIn();
-            // $('.start-video').fadeIn('normal');
         }
 }  
-// function stopVideo() {
-//     player.stopVideo();
-//     $("#player").show();
-// }
 
 $(document).on('click', '.start-video', function () {
     $("#player").show();
     $("#thumbnail_container").fadeOut();
     player.playVideo();
 });
-
-
-// var p = document.getElementById ("player");
-// $(p).hide();
-
-// onPlayerStateChange = function (event) {
-//     if (event.data == YT.PlayerState.ENDED) {
-//         $('.start-video').fadeIn('normal');
-//     }
-// }
-
-// $(document).on('click', '.start-video', function () {
-//     $(this).hide();
-//     $("#player").show();
-//     $("#thumbnail_container").hide();
-//     player.playVideo();
-// });
 
