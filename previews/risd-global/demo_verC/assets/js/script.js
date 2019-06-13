16ba79048e0b4f31b1c1b2d288fbec4e5ca7b7a2
marieotsuka <marieotsuka@users.noreply.github.com>
2019-06-13 00:49:33 -0400

    mockup C

---
+++ previews/risd-global/demo_verC/assets/js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+   console.log('load');
+
+    // $('header').click(function(){
+    //   $('.expanded').eachtoggleClass('dropdown');
+    // });
+    $('.menu').click(function(){
+      $('nav').slideDown();
+      $('header').removeClass('collapsed');
+      $('body').addClass('fixedPos');
+    });
+    $('.close').click(function(){
+      $('nav').slideUp();
+      $('header').addClass('collapsed');
+      $('body').removeClass('fixedPos');
+    });
+
+});
+
+
+$.fn.isInViewport = function() {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
+
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+
+  return elementBottom-500 > viewportTop && elementTop < viewportBottom-500;
+};
+
+$(window).on('resize scroll', function() {
+  $('section').each(function() {
+    if ($(this).isInViewport()) {
+      $(this).addClass('active');
+    } else {
+      $(this).removeClass('active');
+    }
+  });
+});
